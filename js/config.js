34a7f55e98ed0b28e8ec162e2e00de3f1e17a078
7doger <chaofew@163.com>
2018-05-03 19:49:39 +0800

    7doger

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm780217982-422d593e9717539ed85c7c6c', //help.ArisLauncher.com
+  	'm780218014-9eec8d2402ff227751450eb4', //Forum.Mods.ren
+    'm780218017-8dfe694519a4f58d154b4d11', //Applog.ArisLauncher.com
+    'm780218083-55d82a573137d29f86e75a13', //Arislauncher.com
+    'm780218100-f134ec32b27025e271efb4f0', //Aris.bigblog.cn
+    'm780233520-e636a8f585c4b3d99ff50f24', //Store
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
