8d199ad6dcc158e640e18f9b438db95ba4b5f208
7doger <354458414@qq.com>
2019-02-16 12:15:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 var __apiKeys = [
     'm781706242-1c033e595603bf9eca1ec2c3', //www.ArisLauncher.com
     'm780217982-422d593e9717539ed85c7c6c', //help.ArisLauncher.com
-    'm780218017-8dfe694519a4f58d154b4d11', //Applog.ArisLauncher.com
-    'm780218083-55d82a573137d29f86e75a13', //Arislauncher.com
+    'm780218017-8dfe694519a4f58d154b4d11', //applog.ArisLauncher.com
+    'm780218083-55d82a573137d29f86e75a13', //blog.arislauncher.com
     'm780218100-f134ec32b27025e271efb4f0', //plugins.arislauncher.com
 
 ];
