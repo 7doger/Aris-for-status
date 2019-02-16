90fedd75fa5829a817db7ef88e872af4f00cc753
7doger <354458414@qq.com>
2019-02-16 12:08:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@
 var __apiKeys = [
     'm781706242-1c033e595603bf9eca1ec2c3', //www.ArisLauncher.com
     'm780217982-422d593e9717539ed85c7c6c', //help.ArisLauncher.com
-    'm780218014-9eec8d2402ff227751450eb4', //Forum.ArisLauncher.com
     'm780218017-8dfe694519a4f58d154b4d11', //Applog.ArisLauncher.com
     'm780218083-55d82a573137d29f86e75a13', //Arislauncher.com
     'm780218100-f134ec32b27025e271efb4f0', //Aris.bigblog.cn
