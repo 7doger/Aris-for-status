6423b79ed98e0c62e5667a77e34e3470e4373662
7doger <zhangzhangsen@foxmail.com>
2019-04-17 18:22:05 +0800

    2333

--- js/config.js
+++ js/config.js
@@ -2,9 +2,7 @@
 var __apiKeys = [
     'm781706242-1c033e595603bf9eca1ec2c3', //www.ArisLauncher.com
     'm780217982-422d593e9717539ed85c7c6c', //help.ArisLauncher.com
-    'm780218017-8dfe694519a4f58d154b4d11', //applog.ArisLauncher.com
     'm780218083-55d82a573137d29f86e75a13', //blog.arislauncher.com
-    'm780218100-f134ec32b27025e271efb4f0', //plugins.arislauncher.com
 
 ];
 
